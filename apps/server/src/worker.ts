81882ce20af70657d6021e34ae8176db3c96ac67
Exelo <exelo.corp@gmail.com>
2026-03-07 10:03:07 +0100

    feat: add env handling and loader params

--- apps/server/src/worker.ts
+++ apps/server/src/worker.ts
@@ -1,14 +1,19 @@
 import { createRequire } from "node:module";
 
+import { getGameEnv } from "./env";
+
 const bootstrap = async () => {
   const mainPath = process.argv[2] as string;
   const paths = JSON.parse(process.argv[3] as string);
   const { main } = (await createRequire(import.meta.url)(mainPath)) as {
-    main: (options: { files: Map<string, string> }) => Promise<void>;
+    main: (options: {
+      files: Map<string, string>;
+      env: Record<string, string | undefined>;
+    }) => Promise<void>;
   };
 
   console.log("Starting server");
-  await main({ files: new Map(paths) });
+  await main({ files: new Map(paths), env: getGameEnv() });
 };
 
 bootstrap().then();
